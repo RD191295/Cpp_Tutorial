d69d07fcd8545b056ea88e64165c179042e5dddf
Raj Dalsaniya <rajdalsaniya1995@gmail.com>
2021-04-07 15:09:06 +0530

    Add files via upload

---
+++ 2.    Variables/cmake-build-debug/CMakeFiles/Variables.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Variables.dir/main.cpp.obj"
+  "Variables.exe"
+  "Variables.exe.manifest"
+  "Variables.pdb"
+  "libVariables.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Variables.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2.    Variables/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    // characterName and CharacterAge is variable
+    string characterName = "Jay";
+    int characterAge = 17;
+
+    cout << "Hello, My Name is " <<characterName<< endl;
+    cout << "My age is " << characterAge<< endl;
+    return 0;
+}
