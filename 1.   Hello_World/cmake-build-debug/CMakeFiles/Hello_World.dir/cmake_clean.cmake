56cd80522167f3af0238cfe0f3920cfe7a7dc256
Raj Dalsaniya <rajdalsaniya1995@gmail.com>
2021-04-07 15:05:50 +0530

    Add files via upload

---
+++ 1.   Hello_World/cmake-build-debug/CMakeFiles/Hello_World.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello_World.dir/main.cpp.obj"
+  "Hello_World.exe"
+  "Hello_World.exe.manifest"
+  "Hello_World.pdb"
+  "libHello_World.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hello_World.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1.   Hello_World/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, Techgear community!!!" << endl;
+    return 0;
+}
